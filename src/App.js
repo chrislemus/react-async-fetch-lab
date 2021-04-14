7724777153d8c43effb4492ed989625f3ac22fe2
chrislemus <icrislemus@gmail.com>
2021-04-14 13:21:44 -0400

    commit tree b212cf1d7dd4a7bf09344edd5bd8595051ee8e9e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    people: [],
+  };
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then((res) => res.json())
+      .then(({ people }) => this.setState({ people }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>People In Space</h1>
+        <ul></ul>
+      </div>
+    );
+  }
+}
